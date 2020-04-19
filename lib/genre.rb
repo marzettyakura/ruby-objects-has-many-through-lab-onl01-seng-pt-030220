36a46941341ee76171589a42871b4d37735976a3
Yakura Marzett <marzettyakura@yahoo.com>
2020-04-19 19:02:34 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,18 @@
+class Genre
+  attr_accessor :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def songs
+    Song.all.select {|song| song.genre == self}
+  end
+  def artists
+    songs.map {|song| song.artist}
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@all = []
+  def initialize(name, artist, genre)
+    @name, @artist, @genre = name, artist, genre
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+end
